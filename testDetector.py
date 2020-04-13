6ca8cce9271efdced2734b2dcee4c9b3bd8b2767
Jasper Brown <jasperebrown@gmail.com>
2020-04-13 20:04:11 +1000

    added asset

--- testDetector.py
+++ testDetector.py
@@ -24,9 +24,9 @@ from PIL import Image
 # set tf backend to allow memory to grow, instead of claiming everything
 import tensorflow as tf
 
-model_path = '/home/jasper/Datasets/OriginalData/resnet50_coco_best_v2.1.0.h5'
-image_path = '/home/jasper/git/Retinanet-Tutorial/plumsTest.png'
-image_output_path = '/home/jasper/git/Retinanet-Tutorial/plumsTest_detected.png'
+model_path = '~/RetinanetTutorial/RetinanetModels/PlumsInference.h5'
+image_path = '~/RetinanetTutorial/Retinanet-Tutorial/plumsTest.png'
+image_output_path = '~/RetinanetTutorial/Retinanet-Tutorial/plumsTest_detected.png'
 confidence_cutoff = 0.5 #Detections below this confidence will be ignored
 
 def get_session():
